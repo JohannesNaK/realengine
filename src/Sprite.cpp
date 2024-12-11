4319bb34e145b9003ce419e1f35313c70c0525a5
Nomis778 <sijulemi@gmail.com>
2024-12-11 03:58:11 +0100

    base of Sprite class

---
+++ src/Sprite.cpp
@@ -0,0 +1,7 @@
+#include "Sprite.h"
+
+namespace reng{
+
+    Sprite::Sprite(int x, int y, int w, int h): rect{x, y, w, h} {}
+    Sprite::~Sprite(){}
+}
---
+++ src/Sprite.h
@@ -0,0 +1,34 @@
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
+#include <SDL2/SDL.h>
+
+namespace reng
+{
+    class Sprite
+    {
+    public:
+        virtual ~Sprite();
+
+        virtual void mouseDown(const SDL_Event& eve) {}
+        virtual void mouseUp(const SDL_Event& eve) {}
+        virtual void keyDown(const SDL_Event& eve) {}
+        virtual void keyUp(const SDL_Event& eve) {}
+
+        virtual void draw() const = 0;
+        virtual void tick() {};
+
+        const SDL_Rect& getRect() const { return rect; }
+
+    protected:
+        Sprite(int x, int y, int w, int h);
+
+    private:
+        SDL_Rect rect;
+        
+        Sprite(const Sprite& other) = delete;
+        const Sprite& operator=(const Sprite& other) = delete;
+    };
+};
+
+#endif
